d65f50d35c30796300d3593e3fcea643383088c0
sparkle-sparkle <rchaverkate@live.nl>
2013-11-08 11:20:35 +0100

    code cleaning
    extracted the creation of tabs to a generic method

--- src/main/java/nl/sparkle/hellowicket/HelloWicket.java
+++ src/main/java/nl/sparkle/hellowicket/HelloWicket.java
@@ -5,52 +5,56 @@ import java.util.List;
 
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
 import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloWicket extends WebPage {
 	private static final long serialVersionUID = -1099836337542784152L;
+	private static final transient Logger LOGGER = LoggerFactory.getLogger(HelloWicket.class);
 
 	public HelloWicket() {
 		add(new Label("message", "Hello World!"));
-		List<AbstractTab> tabs = createTabs();
+		List<AbstractTab> tabs = addTabs();
 		add(new TabbedPanel<AbstractTab>("tabs", tabs));
 	}
 
-	// TODO: check clean code rules for how to make this code better 
-	private List<AbstractTab> createTabs() {
+	private List<AbstractTab> addTabs() {
 		List<AbstractTab> tabs = new ArrayList<AbstractTab>();
-		AbstractTab tab1 = new AbstractTab(new Model<String>("first tab")) {
-			public Panel getPanel(String panelId) {
-				return new TabPanel1(panelId);
-			}
-		};
-
-		AbstractTab tab2 = new AbstractTab(new Model<String>("second tab")) {
-			public Panel getPanel(String panelId) {
-				return new TabPanel2(panelId);
-			}
-		};
 		
-		AbstractTab tab3 = new AbstractTab(new Model<String>("third tab")) {
-			public Panel getPanel(String panelId) {
-				return new TabPanel3(panelId);
-			}
-		};
-		
-		AbstractTab tab4 = new AbstractTab(new Model<String>("fourth tab")) {
-			public Panel getPanel(String panelId) {
-				return new TabPanel4(panelId);
-			}
-		};
-		
+		AbstractTab tab1 = createTab(TabPanel1.class, "first tab");
 		tabs.add(tab1);
+		
+		AbstractTab tab2 = createTab(TabPanel2.class, "second tab");
 		tabs.add(tab2);
+		
+		AbstractTab tab3 = createTab(TabPanel3.class,"third tab");
 		tabs.add(tab3);
+		
+		AbstractTab tab4 = createTab(TabPanel4.class, "fourth tab");
 		tabs.add(tab4);
+		
 		return tabs;
 	}
+
+	private AbstractTab createTab(final Class<? extends Panel> tabClass, final String tabId) {
+		AbstractTab tab = new AbstractTab(new Model<String>(tabId)) {
+			private static final long serialVersionUID = 1096593543134689283L;
+
+			public Panel getPanel(String panelId) {
+				try {
+					return tabClass.getDeclaredConstructor(String.class).newInstance(panelId);
+				} catch (Exception e) {
+					LOGGER.error(e.getMessage());
+				}
+				return null;
+			}
+		};
+		return tab;
+	}
+	
+	
 }
